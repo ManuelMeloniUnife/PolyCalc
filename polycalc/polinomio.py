9d314ed3796608915023eba6fd089d42979da39a
manuel <manuel.meloni@edu.unife.it>
2025-11-10 19:43:07 +0100

    implementati anche i metodi di analisi, come get_grado: ottiene il grado del polinomio, valuta: valuta il polinomio per un numero intero dato, derivata: calcola la derivata e la restituisce come nuovo polinomio integrale: calcola l'integrale per un intero dato (formula base) e integrale_definito

--- polycalc/polinomio.py
+++ polycalc/polinomio.py
@@ -67,6 +67,8 @@ class Polinomio:
         return Polinomio([c * scalare for c in self.coefficienti])
     
     def divisione(self, altro: "Polinomio") -> Tuple["Polinomio", "Polinomio"]:
+        # TODO sia questo il metodo di divisione A/B, ricordarsi di implementare anche il metodo B/A, 
+        # in modo da poter inserire il doppio button nell'interfaccia grafica del calcolatore.
         if not altro.coefficienti:
             raise ZeroDivisionError("Divisione per polinomio zero")
         if not self.coefficienti:
@@ -94,3 +96,60 @@ class Polinomio:
             for j in range(deg_d + 1):
                 remainder[j + k] -= coeff * divisor[j]
         return Polinomio(quotient), Polinomio(_normalize(remainder))
+
+
+        # ---- ANALISI ----
+
+    def get_grado(self) -> int:
+        if not self.coefficienti:
+            return -1
+        return len(self.coefficienti) - 1
+
+
+    def valuta(self, x: float) -> float:
+        acc = 0.0
+        for c in reversed(self.coefficienti):
+            acc = acc * x + c
+        return acc
+
+
+    def derivata(self) -> "Polinomio":
+        if len(self.coefficienti) <= 1:
+            return Polinomio([])
+        result = [self.coefficienti[i] * i for i in range(1, len(self.coefficienti))]
+        return Polinomio(result)
+
+
+    def integrale(self, costante_di_integrazione: float = 0.0) -> "Polinomio":
+        result = [0.0] * (len(self.coefficienti) + 1)
+        result[0] = costante_di_integrazione
+        for i, c in enumerate(self.coefficienti, start=1):
+            result[i] = self.coefficienti[i - 1] / i
+        return Polinomio(result)
+
+    def integrale_definito(self, x1: float, x2: float) -> float:
+        F = self.integrale(0.0)
+        return F.valuta(x2) - F.valuta(x1)
+
+    # --- AVANZATE ---
+
+    def trova_radici(self) -> List[float]:
+        if self.get_grado() != 2:
+            return []
+        c0 = self.coefficienti[0] if len(self.coefficienti) > 0 else 0.0
+        c1 = self.coefficienti[1] if len(self.coefficienti) > 1 else 0.0
+        c2 = self.coefficienti[2]
+        if isclose(c2, 0.0, abs_tol=TOL):
+            return []
+        delta = c1 * c1 - 4.0 * c2 * c0
+        if delta < -TOL:
+            return []
+        if isclose(delta, 0.0, abs_tol=TOL):
+           return [(-c1) / (2.0 * c2)]
+        sqrt_delta = sqrt(delta)
+        return [(-c1 - sqrt_delta) / (2.0 * c2), (-c1 + sqrt_delta) / (2.0 * c2)]
+    
+
+    # --- UTILITY ---
+
+    
