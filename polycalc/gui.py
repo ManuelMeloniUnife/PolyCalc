ca9d23c3d2cb36c2037108d5aa639b732ab54671
manuel <manuel.meloni@edu.unife.it>
2025-11-14 12:03:24 +0100

    Quasi completata la gui della libreria, dove sono stati inseriti i bottoni che collegano ai metodi per integrali e integrali definiti, inoltre è stata aggiunta una seconda casella di testo per l'inserimento di una variabile (appunto per gli integrali su intervalli finiti).

--- polycalc/gui.py
+++ polycalc/gui.py
@@ -7,7 +7,7 @@ class PolyCalcApp(ttk.Frame):
         super().__init__(master, padding=10)
         self.grid(sticky="nsew")
         master.title("PolyCalc")
-        master.geometry("650x500")
+        master.geometry("900x700")
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
 
@@ -17,6 +17,7 @@ class PolyCalcApp(ttk.Frame):
         self.p1_var = tk.StringVar(value="4, -1, 5")
         self.p2_var = tk.StringVar(value="1, 2")
         self.x_var = tk.StringVar(value="2")
+        self.y_var = tk.StringVar(value="3")
 
 
         # --- INPUT UTENTE ---
@@ -31,7 +32,10 @@ class PolyCalcApp(ttk.Frame):
         ttk.Label(frm_inputs, text="Polinomio B").grid(row=1, column=0, sticky="w", padx=4, pady=4)
         ttk.Entry(frm_inputs, textvariable=self.p2_var).grid(row=1, column=1, sticky="ew", padx=4, pady=4)
         ttk.Label(frm_inputs, text="x").grid(row=2, column=0, sticky="w", padx=4, pady=4)
-        ttk.Entry(frm_inputs, textvariable=self.x_var, width=12).grid(row=2, column=1, sticky="w", padx=4, pady=4)
+        ttk.Entry(frm_inputs, textvariable=self.x_var, width=12).grid(row=2, column=1, sticky="w", padx=2, pady=4)
+        ttk.Label(frm_inputs, text="y").grid(row=3, column=0, sticky="w", padx=4, pady=4)
+        ttk.Entry(frm_inputs, textvariable=self.y_var, width=12).grid(row=3, column=1, sticky="w", padx=2, pady=4)
+
 
 
         # --- RIGA DI BOTTONI COLLEGATI AI METODI ---
@@ -46,15 +50,20 @@ class PolyCalcApp(ttk.Frame):
         ttk.Button(frm_btn, text="SUB A-B", command=self._sottrazioneAB).grid(row=0, column=1, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="SUB B-A", command=self._sottrazioneBA).grid(row=1, column=1, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="MULT A*B", command=self._moltiplicazione).grid(row=0, column=2, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="MULT A*x", command=self._moltiplicazione_scalareA).grid(row=1, column=2, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="MULT B*x", command=self._moltiplicazione_scalareB).grid(row=2, column=2, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="DIV A/B", command=self._divisioneAB).grid(row=0, column=3, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="DIV B/A", command=self._divisioneBA).grid(row=1, column=3, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="DERIV A'", command=self._derivataA ).grid(row=0, column=4, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="DERIV B'", command=self._derivataB ).grid(row=1, column=4, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="SOLVE A(x)", command=self._valutaA).grid(row=0, column=5, sticky="ew", padx=2, pady=2)
         ttk.Button(frm_btn, text="SOLVE B(x)", command=self._valutaB).grid(row=1, column=5, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="INTEG A(x)", command=self._integraleA).grid(row=0, column=6, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="INTEG B(x)", command=self._integraleB).grid(row=1, column=6, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="INTEG A(x -> y)", command=self._integraleAxy).grid(row=0, column=7, sticky="ew", padx=2, pady=2)
+        ttk.Button(frm_btn, text="INTEG B(x -> y)", command=self._integraleBxy).grid(row=1, column=7, sticky="ew", padx=2, pady=2)
 
         # --- CELLA TESTO DI OUTPUT ---
-
 
         frm_out = ttk.LabelFrame(self, text="Output")
         frm_out.grid(row=2, column=0, sticky="nsew", padx=4, pady=4)
@@ -119,7 +128,24 @@ class PolyCalcApp(ttk.Frame):
             self._append(f"MUL [A*B] = {r}")
         except Exception as e:
             messagebox.showerror("Errore", str(e))
+    
+    def _moltiplicazione_scalareA(self) -> None:
+        try:
+            p, _ = self._get_polinomi()
+            x = float(self.x_var.get())
+            r = p.moltiplicazione_per_scalare(x)
+            self._append(f"MUL [A*{x}] = {r}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
 
+    def _moltiplicazione_scalareB(self) -> None:
+        try:
+            _, p = self._get_polinomi()
+            x = float(self.x_var.get())
+            r = p.moltiplicazione_per_scalare(x)
+            self._append(f"MUL [A*{x}] = {r}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))        
 
     def _divisioneAB(self) -> None:
         try:
@@ -168,6 +194,42 @@ class PolyCalcApp(ttk.Frame):
             x = float(self.x_var.get())
             v = p.valuta(x)
             self._append(f"A({x}) = {v:g}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
+    
+    def _integraleA(self) -> None:
+        try:
+            p, _ = self._get_polinomi()
+            r = p.integrale()
+            self._append(f"INT [A(x)] = {r}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
+
+    def _integraleB(self) -> None:
+        try:
+            _, p = self._get_polinomi()
+            r = p.integrale()
+            self._append(f"INT [A(x)] = {r}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
+
+    def _integraleAxy(self) -> None:
+        try:
+            p, _ = self._get_polinomi()
+            x = float(self.x_var.get())
+            y = float(self.y_var.get())
+            r = p.integrale_definito(x,y)
+            self._append(f"INT [A(x)] = {r}")
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
+    
+    def _integraleBxy(self) -> None:
+        try:
+            _, p = self._get_polinomi()
+            x = float(self.x_var.get())
+            y = float(self.y_var.get())
+            r = p.integrale_definito(x,y)
+            self._append(f"INT [A(x)] = {r}")
         except Exception as e:
             messagebox.showerror("Errore", str(e))
 
